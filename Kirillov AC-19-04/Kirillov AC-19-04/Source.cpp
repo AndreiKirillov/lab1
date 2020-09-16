6e47d478380d759f7ecb81b8fe1f79749206ec42
AndreiKirillov <AndreiKirillov@users.noreply.github.com>
2020-09-16 15:32:55 +0300

    d

---
+++ Kirillov AC-19-04/Kirillov AC-19-04/Source.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct pipe
+{
+	string id;
+	int length;
+	int diametr;
+	bool remont;
+};
+
+struct KS
+{
+	string id;
+	string name;
+	int number_ceh;
+	int number_ceh_inWork;
+	int efficiency;
+};
+
+int main()
+{
+
+	return 0;
+}
