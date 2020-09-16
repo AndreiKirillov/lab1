b9af4266b26da0699c34ff7048bfcea365fd4271
AndreiKirillov <AndreiKirillov@users.noreply.github.com>
2020-09-16 16:31:51 +0300

    Вывод

--- Kirillov AC-19-04/Kirillov AC-19-04/Source.cpp
+++ Kirillov AC-19-04/Kirillov AC-19-04/Source.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 struct pipe
 {
-	string id;
+	int id;
 	int length;
 	int diametr;
 	bool remont;
@@ -19,8 +19,22 @@ struct KS
 	int efficiency;
 };
 
+pipe NewPipe()
+{
+	pipe pipe1;
+	cout << "¬ведите длину трубы:";
+	cin >> pipe1.length;
+	cout << "¬ведите диаметр трубы:";
+	cin >> pipe1.diametr;
+	pipe1.id = 0;
+	pipe1.remont = false;
+	return pipe1;
+}
+
 int main()
 {
-
+	setlocale(LC_ALL, "Russian");
+	pipe p1 = NewPipe();
+	cout << "ƒлинна данной трубы:" << p1.length << endl << "ƒиаметр данной трубы:" << p1.diametr << endl;
 	return 0;
 }
