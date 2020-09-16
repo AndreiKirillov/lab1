855d386e9d936f4839bac35605da553ca6808110
AndreiKirillov <AndreiKirillov@users.noreply.github.com>
2020-09-16 17:04:15 +0300

    ф

--- Kirillov AC-19-04/Kirillov AC-19-04/Source.cpp
+++ Kirillov AC-19-04/Kirillov AC-19-04/Source.cpp
@@ -31,10 +31,18 @@ pipe NewPipe()
 	return pipe1;
 }
 
+void ChangeStatus(bool b)
+{
+	b = !b;
+}
+
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	pipe p1 = NewPipe();
 	cout << "Длинна данной трубы:" << p1.length << endl << "Диаметр данной трубы:" << p1.diametr << endl;
+	cout <<"Статус ремонта:" << p1.remont << endl;
+	ChangeStatus(p1.remont);
+	cout << "Новый статус ремонта:" << p1.remont << endl;
 	return 0;
 }
